81c2709dfe0a16ea0af50aa3a12d879fcbafcdb9
brey <breyiannis@gmail.com>
2018-03-05 17:14:03 +0100

    create a new xarray Dataset for the d3d results

--- pyPoseidon/utils/data.py
+++ pyPoseidon/utils/data.py
@@ -2,21 +2,14 @@ import numpy as np
 import pickle
 import os
 from pyPoseidon.model.grid import *
-from pyPoseidon.model.dep import *
-from pyPoseidon.model import mdf
 from pyPoseidon.utils.vis import *
+from pyPoseidon.utils.obs import obs
 import pyresample
-from pyresample import bilinear
 import pandas as pd
 import datetime
 from netCDF4 import Dataset
-import subprocess
-import tempfile
-import time
-import folium
 from pyPoseidon.utils.get_value import get_value
 import xarray
-import shutil
 import holoviews as hv
 import geoviews as gv
 from cartopy import crs
@@ -29,72 +22,6 @@ FFWriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264','-pix
    
 class data:   
        
-                                    
-    def animaker(self,var,vmax,vmin,title,units,step,savepath=None):
-              
-        flist=[]
-       
-        k=0
-
-        timef = []
-        
-        plt.ioff()
-
-        for folder in self.folders:
-
-          # read netcdf
-          dat=Dataset(folder+'/'+'trim-'+self.info['tag']+'.nc')
-                   
-          #read reference date
-          inp, order = mdf.read(folder+'/'+self.info['tag']+'.mdf')
-          refdat = inp['Itdate']
-          refdate = datetime.datetime.strptime(refdat,'%Y-%m-%d')
-          t = dat.variables['time'][:step]
-          label = []
-          for it in range(t.shape[0]):
-                label.append(refdate+datetime.timedelta(seconds=np.int(t[it])))
-                timef.append(refdate+datetime.timedelta(seconds=np.int(t[it])))
-          
-          h = dat.variables[var][:step,1:-1,1:-1]
-          ha = np.transpose(h,axes=(0,2,1)) #transpose lat/lon
-          ww = np.broadcast_to(self.w == True, ha.shape) # expand the mask in time dimension
-          z = np.ma.masked_where(ww==True,ha) #mask land
-          
-          a = anim(self.xh,self.yh,z,title=title,label=label,units=units,vrange=[vmin,vmax])
-          
-          with tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) as temp: # open temp file
-              a.save(temp.name, writer = FFWriter)      
-              flist.append(temp.name)
-          
-          k+=1
-          
-        self.time = timef
-        #save list 
-        with tempfile.NamedTemporaryFile(mode='w+t', suffix='.txt', delete=False) as temp:
-              listname=temp.name
-              for name in flist: temp.write('file   {}\n'.format(name))
-         
-        #merge clips   
-        outfile = tempfile.NamedTemporaryFile(suffix='.mp4', delete=False) # open temp file
-        ex=subprocess.Popen(args=['ffmpeg -y -f concat -safe 0 -i {} -c copy {}'.format(listname,outfile.name)], cwd=tempfile.gettempdir(), shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1)
-     #       out = video(temp.name, 'mp4')
-
-        time.sleep(5)
-
-        #cleanup
-        for name in flist:
-            os.remove(name)
-        os.remove(listname)
-
-        plt.ion()
-#       print outfile.name
-#       print os.path.isfile(outfile.name) 
-#       print os.path.getsize(outfile.name)
-        if savepath:
-           shutil.move(outfile.name,savepath)
-           return
-        else:
-           return video(outfile.name, 'mp4')
         
     def __init__(self,folders,**kwargs):
         
@@ -103,101 +30,105 @@ class data:
         with open(self.folders[0]+'/info.pkl', 'r') as f:
                           self.info=pickle.load(f)  
                         
-        grid=Grid.fromfile(self.folders[0]+'/'+self.info['tag']+'.grd')
+        grid=r2d.read_file(self.folders[0]+'/'+self.info['tag']+'.grd')
             
-        deb=Dep.read(self.folders[0]+'/'+self.info['tag']+'.dep',grid.shape)
+        deb=np.loadtxt(self.folders[0]+'/'+self.info['tag']+'.dep')
         
-        self.dem = deb
+        #create mask
+            
+        d=deb[1:-1,1:-1]
+        self.w=d==-999.
+        
+        b=deb[:-1,:-1]
+        b[b==-999.]=np.nan
+        
+        self.dem = xr.Dataset({'bathymetry': (['latitude', 'longitude'], b)},
+                    coords={'longitude': ('longitude', grid.lons[0,:]),   
+                            'latitude': ('latitude', grid.lats[:,0])})
         
         self.grid = grid
             
-        #create mask
-            
-        d=deb.val[1:-1,1:-1]
-        self.w=np.isnan(d)
             
             # read array
-        dat=Dataset(self.folders[0]+'/'+'trim-'+self.info['tag']+'.nc') 
-            
-        xz = dat.variables['XZ'][:]
-        xz = xz.T[1:-1,1:-1]
-        yz = dat.variables['YZ'][:]   
-        yz = yz.T[1:-1,1:-1]  
-            
+        
+        nfiles = [folder +'/'+'trim-'+self.info['tag']+'.nc' for folder in folders]
+        
+        ds = xarray.open_mfdataset(nfiles)
+                
+        #grid points
+        xg = ds.XCOR.values.T
+        yg = ds.YCOR.values.T   
+        self.x = xg
+        self.y = yg
+        
+        self.dx = self.x[0,1]-self.x[0,0]
+        self.dy = self.y[1,0]-self.y[0,0]        
+        
+        #pressure points       
+        xz = self.x - self.dx/2.
+        yz = self.y - self.dy/2.
+        
+        xz = xz[1:-1,1:-1]  
+        yz = yz[1:-1,1:-1]  
+        
         self.xh = np.ma.masked_array(xz, self.w) #mask land
         self.yh = np.ma.masked_array(yz, self.w)
         
-        self.dx = self.xh.data[0,1]-self.xh.data[0,0]
-        self.dy = self.yh.data[1,0]-self.yh.data[0,0]        
-         
-        self.variables = dat.variables.keys()  
-                 
-        self.ds = xarray.open_mfdataset(nfiles)
+        #velocity points
+        xu = self.x - self.dx/2.
+        yv = self.y - self.dy/2.
+       
+        self.times=ds.time.values
+        
+        
+        # get S1
+        h =  ds.S1[:,1:-1,1:-1]
+        ha = h.transpose(h.dims[0],h.dims[2],h.dims[1]) #transpose lat/lon
+        ww = np.broadcast_to(self.w == True, ha.shape) # expand the mask in time dimension
+        z = np.ma.masked_where(ww==True,ha) #mask land
+                
+        # get U1
+        h =  ds.U1[:,0,1:-1,1:-1]
+        ha = h.transpose(h.dims[0],h.dims[2],h.dims[1]) #transpose lat/lon
+        ww = np.broadcast_to(self.w == True, ha.shape) # expand the mask in time dimension
+        zu = np.ma.masked_where(ww==True,ha) #mask land
+                
+        # get V1
+        h =  ds.V1[:,0,1:-1,1:-1]
+        ha = h.transpose(h.dims[0],h.dims[2],h.dims[1]) #transpose lat/lon
+        ww = np.broadcast_to(self.w == True, ha.shape) # expand the mask in time dimension
+        zv = np.ma.masked_where(ww==True,ha) #mask land
+                
+        
+        self.vars=xr.Dataset({'S1':(['time','YZ','XZ'], z),
+                       'U1':(['time','YU','XU'], zu),
+                       'V1':(['time','YV','XV'], zv)},                       
+                     coords={'XZ':(self.xh[0,:]),'YZ':(self.yh[:,0]),
+                             'XU':(xu[0,1:-1]),'YU':(self.yh[:,0]),
+                             'XV':(self.xh[0,:]),'YV':(yv[1:-1,0]),                   
+                     'time':self.times})
+                     
+        self.obs = obs(**self.info)
                 
     def hview(self,var,**kwargs):
         
-        vart = np.squeeze(self.ds[var].values)
-        vart = np.transpose(vart,axes=(0,2,1))
-
-        lon = self.ds.XZ.values[1:-1,1:-1].T[0,:]
-
-        lat = self.ds.YZ.values[1:-1,1:-1].T[:,0]
-
-        times=self.ds.time.values
-
-        ds=xr.Dataset({var:(['time','latitude','longitude'], vart[:,1:-1,1:-1])},
-                     coords={'longitude':(lon),'latitude':(lat),'time':times})
-
-        return hv.Dataset(ds,kdims=['time','longitude','latitude'],vdims=[var])
+        return hv.Dataset(self.vars[var])
    
    
     def gview(self,var,**kwargs):
         
-        vart = np.squeeze(self.ds[var].values)
-        vart = np.transpose(vart,axes=(0,2,1))
-
-        lon = self.ds.XZ.values[1:-1,1:-1].T[0,:]
-
-        lat = self.ds.YZ.values[1:-1,1:-1].T[:,0]
-
-        times=self.ds.time.values
-
-        ds=xr.Dataset({var:(['time','latitude','longitude'], vart[:,1:-1,1:-1])},
-                     coords={'longitude':(lon),'latitude':(lat),'time':times})
-
-        return gv.Dataset(ds,kdims=['time','longitude','latitude'],vdims=[var])
+        return gv.Dataset(self.vars[var])
     
                                    
-    def movie(self,var,**kwargs):
-         
-        step = kwargs.get('step', None)
-        savepath = kwargs.get('savepath', None)
-         
-        vmax=0.
-        vmin=0. 
-        for folder in self.folders:
-
-            dat=Dataset(folder+'/'+'trim-'+self.info['tag']+'.nc')
-            vmax = max(dat.variables[var][:step].max(), vmax)
-            vmin = min(dat.variables[var][:step].min(), vmin)
-            title = dat.variables[var].long_name
-            units = dat.variables[var].units
-            
-         
-        return self.animaker(var,vmax,vmin,title,units,step,savepath=savepath)
+    def frames(self,var,**kwargs):
         
-    
-   # def frame(self,slot):
-   
-    def maxval(self,var,**kwargs):
-       
-       step = kwargs.get('step', None)
-       
-       vmax = np.amax(np.transpose(otp.ds.S1.values,axes=(0,2,1)), axis=0)           
-   
-       return np.ma.masked_array(vmax,self.xh.mask)
-       
-               
+        if len(var) == 1 :  
+            return contour(self.xh,self.yh,self.vars[var[0]],self.times,**kwargs)
+        elif len(var) == 2:
+            return quiver(self.xh,self.yh,self.vars[var[0]],self.vars[var[1]],self.times,**kwargs)
+            
+            
+                   
 class point:
     
     def __init__(self,**kwargs):
@@ -205,7 +136,6 @@ class point:
         self.lon = kwargs.get('lon', None) 
         self.lat = kwargs.get('lat', None) 
         self.data = kwargs.get('data', None)
-        self.name = kwargs.get('name', None)
             
     def tseries(self,**kwargs):
         
@@ -267,23 +197,6 @@ class point:
         pdata = pd.concat(frames)
         setattr(self, var, pdata.set_index(['time']))
         
-    def on_map(self,**kwargs):
-        
-            from folium.features import DivIcon
-        
-            name = get_value(self,kwargs,'name',None)
-            marker = kwargs.get('marker', None)
-                        
-            if marker == None:
-                attrs = folium.Marker([self.lat,self.lon],popup=name)
-            elif (marker == 'circle'):
-                attrs = folium.CircleMarker(location=[self.lat,self.lon],
-                                radius = 5,
-                                fill=True,
-                                color='black',
-                                popup=name) 
-        
-            return attrs
         
 class node:
     
@@ -385,77 +298,3 @@ class node:
      
         return frames 
           
-    def on_map(self,**kwargs):
-        
-        from folium.features import DivIcon
-        
-        dyy = self.data.dy/2 # staggered shift
-        dxx = self.data.dx/2
-        
-        dfp=pd.DataFrame([{'ID':self.name,'kind':'pressure point','i':self.i,'j':self.j,'lon':self.ilon,'lat':self.ilat}])
-
-        dfp = dfp.set_index('ID')
-
-        htmlp = dfp.to_html(classes='table table-striped table-hover table-condensed table-responsive')#,index=None)
-        
-        popup_p = folium.Popup(htmlp)
-                
-        attrs=[]
-        
-        cell=folium.features.RectangleMarker(bounds=[[self.ilat-dyy, self.ilon-dxx], [self.ilat+dyy, self.ilon+dxx]], \
-                                color='black', fill_color='black', fill_opacity=.4, popup='cell')
-        attrs.append(cell)
-        
-        pp = folium.CircleMarker(location=[self.ilat,self.ilon],
-                            radius = 6,
-                            fill=True,
-                            color='blue',
-                            popup=popup_p)
-                            
-        attrs.append(pp)
-        
- 
-        dfg=pd.DataFrame([{'ID':self.name, 'kind':'grid point','i':self.i,'j':self.j,'lon':self.ilon+dxx,'lat':self.ilat+dyy}])
-
-        dfg = dfg.set_index('ID')
- 
-        htmlg = dfg.to_html(classes='table table-striped table-hover table-condensed table-responsive')#,index=self.name)
- 
-        popup_g = folium.Popup(htmlg)
- 
-                
-        gp = folium.CircleMarker(location=[self.ilat+dyy,self.ilon+dxx],
-                            radius = 3,
-                            fill=True,
-                            color='black',
-                            popup=popup_g) 
-                            
-        attrs.append(gp)
-        
-        
-        return attrs
-        
-                
-#        folium.map.Marker(
-#             [self.lat,self.lon],
-#             icon=DivIcon(
-#                 icon_size=(150,36),
-#                 icon_anchor=(0,0),
-#                 html='<div style="font-size: 12pt">Pressure point</div>',
-#                 )
-#             ).add_to(mapa)
-         
-#        folium.map.Marker(
-#             [self.lat+self.data.dy,self.lon+self.data.dx],
-#             icon=DivIcon(
-#                 icon_size=(150,12),
-#                 icon_anchor=(0,0),
-#                 html='<div style="font-size: 12pt">Grid point</div>',
-#                 )
-#             ).add_to(mapa)
-             
-             
-                            
-        return mapa    
-        
-        
--- pyPoseidon/utils/vis.py
+++ pyPoseidon/utils/vis.py
@@ -2,110 +2,110 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, shiftgrid
 from matplotlib import animation,rc
+from cartopy import crs
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
 
 rc('animation',html='html5')
+plt.rcParams["animation.html"] = "jshtml"
+plt.rcParams['animation.embed_limit'] = '200.'
         
 FFWriter = animation.FFMpegWriter(fps=30, extra_args=['-vcodec', 'libx264','-pix_fmt','yuv420p'])
-
-def map(x, y, z, title=None,label=None,**kwargs):
-    
-    parallels=np.arange(-90.,90.,5.)
-    meridians=np.arange(0.,360.,5.)
-    
-    minlon = kwargs.get('minlon', x.min())
-    maxlon = kwargs.get('maxlon', x.max())
-    minlat = kwargs.get('minlat', y.min())
-    maxlat = kwargs.get('maxlat', y.max())
-
-    ticks = kwargs.get('ticks', True)
-    png = kwargs.get('png', False)
-    path = kwargs.get('path', './')
-# Create map
-    m = Basemap(projection='cyl', llcrnrlat=minlat,urcrnrlat=maxlat,llcrnrlon=minlon, urcrnrlon=maxlon,resolution='l')
-    fig = plt.figure(figsize=(10,8))
-    cs = m.contourf(x,y,z,cmap=plt.cm.jet)
-    if ticks:
-       m.drawparallels(parallels,labels=[False,True,True,False])
-       m.drawmeridians(meridians,labels=[True,False,False,True])
-    
-    m.drawcoastlines()
-    m.drawmapboundary()
-    plt.title(title)
-    cbar = plt.colorbar(orientation='horizontal', extend='both')
-    cbar.ax.set_xlabel(label)
-
-# Save figure (without 'white' borders)
-    if png : plt.savefig(path, bbox_inches='tight')
-    plt.show(block=False)
-
-
-def anim(x,y,z,title=None,label=None,units=None,**kwargs):
-    
+ 
+def contour(grid_x,grid_y,z,t,**kwargs):
+    fig, ax = plt.subplots(figsize=(12,8)) 
     [v1,v2] = kwargs.get('vrange', [None,None])
     
-    v = np.linspace(v1, v2, 15, endpoint=True)
+    title = kwargs.get('title', None)
     
-    def animate(i):
-        H.set_array(z[i,:,:])
-        return H
+    if not v1:
+        v1=z.min()
+        v2=z.max()
     
-    parallels=np.arange(-90.,90.,5.)
-    meridians=np.arange(0.,360.,5.)
-    
-    minlon = kwargs.get('minlon', x.min())
-    maxlon = kwargs.get('maxlon', x.max())
-    minlat = kwargs.get('minlat', y.min())
-    maxlat = kwargs.get('maxlat', y.max())
+    v=np.linspace(z.min(),z.max(),10,endpoint=True)
+    ## CHOOSE YOUR PROJECTION
+ #   ax = plt.axes(projection=ccrs.Orthographic(grid_x.mean(), grid_y.mean()))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    # Limit the extent of the map to a small longitude/latitude range.
 
-    latlons = kwargs.get('latlons', True)
-    png = kwargs.get('png', False)
-    path = kwargs.get('path', './')
-    # Create map
-    m = Basemap(projection='cyl', llcrnrlat=minlat,urcrnrlat=maxlat,llcrnrlon=minlon, urcrnrlon=maxlon,resolution='l')
-    
-    fig = plt.figure(figsize=(12,8))
-    ax=ax = plt.gca()
-  
-    if latlons:
-       m.drawparallels(parallels,labels=[False,True,True,False])
-       m.drawmeridians(meridians,labels=[True,False,False,True])
-    
-    m.drawcoastlines()
-    m.drawmapboundary()
-    
-    # ims is a list of lists, each row is a list of artists to draw in the
-    # current frame; here we are animating three artists, the contour and 2 
-    # annotatons (title), in each frame
+    ax.set_aspect('equal')
     ims = []
-    for i in range(len(z[:,0,0])):
-        im = m.contourf(x,y,z[i,:,:],v,vmin=v1,vmax=v2,latlon=True)
+    for i in range(len(t)):
+        im = ax.contourf(grid_x, grid_y, z[i,:,:], v, vmin=v1, vmax=v2, transform=ccrs.PlateCarree())
+#        im = ax.contourf(x,y,z[i,:,:],v,vmin=v1,vmax=v2,latlon=True)
         add_arts = im.collections
-        text = 'time={}'.format(label[i])
-        te = ax.text(90, 90, text)
-        an = ax.annotate(text, xy=(-0.25, -0.15), xycoords='axes fraction')
-        ims.append(add_arts + [te,an])
-    
-    #cbar_ax = fig.add_axes([0.05, 0.05, 0.85, 0.05])    
-    cbar = plt.colorbar(ticks=v,orientation='horizontal', extend='both')#,fraction=0.046, pad=0.04)
-    if units : cbar.ax.set_xlabel(units)
+        text = 'time={}'.format(t[i])
+        #te = ax.text(90, 90, text)
+        an = ax.annotate(text, xy=(0.05, 1.05), xycoords='axes fraction')
+        ims.append(add_arts + [an])
+    if title : ax.set_title(title) 
+    #ax.set_global()
+    ax.coastlines('50m')
+    #ax.set_extent([grid_x.min(), grid_x.max(), grid_y.min(), grid_y.max()])
 
-    if title : plt.title(title)
-    
-    anim = animation.ArtistAnimation(fig, ims, interval=200, blit=False,repeat=False)
-    
-#    anim = animation.FuncAnimation(fig,animate,frames=z.shape[0],interval=200, blit=False,repeat=False)
-        
-    save = kwargs.get('save', False)
-    path = kwargs.get('path', './')
-    
-    plt.close('all')
 
-    if save : 
-        anim.save(path, writer = FFWriter)      
+#cbar_ax = fig.add_axes([0.05, 0.05, 0.85, 0.05])    
+    cbar = fig.colorbar(im,ticks=v,orientation='vertical', extend='both')#,fraction=0.046, pad=0.04)
+#plt.colorbar()
+
+    v = animation.ArtistAnimation(fig, ims, interval=200, blit=False,repeat=False)
+     
+    plt.close()
+    
+    if 'savepath' in kwargs.keys(): 
+        savepath = kwargs.get('path', './')
+        v.save(path, writer = FFWriter)      
     else:
-        return anim
- 
- 
+        return v
+
+
+def update_quiver(num, Q, U, V, step):
+    """updates the horizontal and vertical vector components by a
+    fixed increment on each frame
+    """
+    Q.set_UVC(U[num,::step,::step],V[num,::step,::step])
+
+    return Q,
+    
+    
+def quiver(X,Y,U,V,t,**kwargs):
+    
+    fig = plt.figure(figsize=(16,12))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    crs = ccrs.PlateCarree()
+    ax.set_aspect('equal')
+
+    land_50m = cfeature.NaturalEarthFeature('physical', 'land', '50m',
+                                        edgecolor='face',
+                                        facecolor=cfeature.COLORS['land'],zorder=0)
+
+    sea_50m = cfeature.NaturalEarthFeature('physical', 'ocean', '50m',
+                                            edgecolor='face',
+                                            facecolor=cfeature.COLORS['water'], zorder=0)
+
+    title = kwargs.get('title', None)
+
+    ax.coastlines('50m')
+    ax.add_feature(land_50m)
+    ax.add_feature(sea_50m)
+
+    scale = kwargs.get('scale', 1.) # change accordingly to fit your needs
+    step = kwargs.get('step', 1) # change accordingly to fit your needs
+
+    Q = ax.quiver(X[::step,::step], Y[::step,::step], U[0,::step,::step], V[0,::step,::step], pivot='mid', color='k', angles='xy', scale_units='xy', scale = scale, transform=crs)
+
+    ax.set_xlim(X.min(), X.max())
+    ax.set_ylim(Y.min(), Y.max())
+    ax.set_title(title) 
+    #ax.set_global()
+
+    plt.close()
+    # you need to set blit=False, or the first set of arrows never gets
+    # cleared on subsequent frames
+    return animation.FuncAnimation(fig, update_quiver, fargs=(Q, U, V, step),
+                               interval=np.size(t), blit=False, repeat=False)    
+    
  
 def video(fname, mimetype):
      """Load the video in the file `fname`, with given mimetype, and display as HTML5 video.
