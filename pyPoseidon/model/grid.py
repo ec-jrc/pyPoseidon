1a9eaefb7817f9777f5fed825076f72ae7faeb3e
brey <breyiannis@gmail.com>
2018-03-05 11:46:58 +0100

    New grid class
    Eliminating Delteres code, prepare for jigsaw grid generation

--- pyPoseidon/model/grid.py
+++ pyPoseidon/model/grid.py
@@ -1,103 +1,70 @@
 import numpy as np
 import datetime
+import xarray as xr
+import pandas as pd
 
-class Grid(object):
-    """Create a Delft3D grid file
-    # Create an empty grid
-    grid = Grid()
-    # Load a grid from file
-    grid = Grid.fromfile('filename.grd')
-    # Write grid to file
-    Grid.write(grid,'filename.grd')
+
+class r2d:
+    """Regular 2d grid for d3d
     """
     def __init__(self, **kwargs):
-        self.properties = kwargs.get('properties', {'Coordinate System': 'Spherical', 'alfori': 0.0, 'xori': 0.0, 'yori': 0.0})
-        self.shape = kwargs.get('shape', None)
-        self.x     = kwargs.get('x', None)
-        self.y     = kwargs.get('y', None)
-
+        
+        gx    = kwargs.get('x', None)
+        gy    = kwargs.get('y', None)
+        attrs = kwargs.get('attrs', {'Coordinate System': 'Spherical', 'alfori': 0.0, 'xori': 0.0, 'yori': 0.0})
+        
+        g = xr.Dataset({'lons': (['x', 'y'], gx),   
+                        'lats': (['x', 'y'], gy)})
+        g.attrs = attrs
+        
+        self.grid = g
+        
 
     @staticmethod
-    def fromfile(filename, **kwargs):
-        grid = Grid()
-        grid.filename = filename
-        grid.shape    = None
-        rows = []
-        with open(filename) as f:
-            for line in f:
-                line = line.strip()
-                # skip comments and empty lines
-                if line.startswith('*') or not line:
-                    continue
-                elif '=' in line:
-                    key, value = line.split('=')
-                    if 'Coordinate' in key:
-                        grid.properties[key.strip()] = value.strip()
-                    if 'ETA' in key:
-                        row = value.split()
-                        n, row = row[0], row[1:]
-                        while len(row) < grid.shape[1]:
-                            line = f.next()
-                            row.extend(line.split())
-                        rows.append(row)
-                # Read grid size
-                elif grid.shape == None:
-                    # line should contain size
-                    # convert to nrow x ncolumns
-                    grid.shape = tuple(np.array(line.split()[::-1], dtype='int'))
-                    assert len(grid.shape) == 2, "Expected shape (2,), got {}, (subgrids not supported)".format(grid.shape)
-                    # also read next line
-                    line = f.next()
-                    grid.properties['xori'], grid.properties['yori'], grid.properties['alfori'] = np.array(line.split(), dtype='float')
-        # rows now contains
-        #[X
-        # Y]
-        data = np.array(rows, dtype="double")
-        assert (data.shape[0], data.shape[1]) == (grid.shape[0]*2, grid.shape[1]), \
-        "Expected shape of data:{} , got {}".format((grid.shape[0]*2, grid.shape[1]), (data.shape[0], data.shape[1]))
-        X, Y = data[:grid.shape[0],:], data[grid.shape[0]:,:]
-        grid.x = np.ma.MaskedArray(X,mask=X==0.) # apply standard nodatavalue of 0
-        grid.y = np.ma.MaskedArray(Y,mask=Y==0.) # apply standard nodatavalue of 0
-        return grid
+    def read_file(filename, **kwargs):
+
+        header=pd.read_csv(filename,nrows=3,header=None,comment='*')
+        cs = header.loc[0,0].split('=')[1].strip()
+        ni,nj = header.loc[1,0].split(' ')
+        ni,nj = int(ni),int(nj)
+        alfori, xori, yori = header.loc[2,0].split(' ')
+        
+        d = pd.read_csv(filename,header=2,comment='*',delim_whitespace=True,engine='python',na_values='ETA=')
+        d = d.reset_index()
+        data = d.values[~np.isnan(d.values)]
+        data=np.array(data)
+        data = data.reshape(2,nj,ni+1) # including the row index
+        #clean up the row index
+        data = data[:,:,1:]
+        
+        lons=data[0,:,:]
+        lats=data[1,:,:]      
+          
+        g = xr.Dataset({'lons': (['x', 'y'], lons),   
+                        'lats': (['x', 'y'], lats)})
+                        
+        g.attrs = {'Coordinate System': cs, 'alfori': alfori, 'xori': xori, 'yori': yori}
+        
+        return g
+            
                     
-    def write(self, filename, **kwargs):
+    def to_file(self, filename, **kwargs):
+        with open(filename,'w') as f:
+            f.write('Coordinate System= {}\n'.format(self.grid.attrs['Coordinate System']))
+            f.write('{} {}\n'.format(self.grid.lons.shape[1],self.grid.lons.shape[0]))
+            f.write('{} {} {}\n'.format(self.grid.attrs['xori'],self.grid.attrs['yori'],self.grid.attrs['alfori']))
+            for i in range(self.grid.lons.shape[0]):
+                f.write('ETA=  {} '.format(i+1))
+                f.write(' '.join(map(str, self.grid.lons[i,:].values)))
+                f.write('\n')
+            for i in range(self.grid.lats.shape[0]):
+                f.write('ETA=  {} '.format(i+1))
+                f.write(' '.join(map(str, self.grid.lats[i,:].values)))
+                f.write('\n')
 
-        f = open(filename,'w')
         
-        mn = np.shape(self.x)
 
-        f.write('* Created at ' + str(datetime.datetime.now()) + '\n')
-        f.write('* by OpenEarth Tools \n')
-        f.write('* $HeadURL: https://svn.oss.deltares.nl/repos/openearthtools/trunk/python/OpenEarthTools/openearthtools/io/delft3d/grid.py $\n')
-        f.write('* $Revision: 10039 $\n')
-        f.write('Coordinate System=' + self.properties['Coordinate System'] + '\n')
-        f.write(str(mn[1]) + ' ' + str(mn[0]) + '\n')
-        f.write(str(self.properties['xori']) + ' ' + str(self.properties['yori']) + ' ' + str(self.properties['alfori']) + '\n')
-        
-        nrow = int(np.ceil(mn[1]/5)) # max 5 m-values per line
-        res=mn[1]%5
-        if res != 0 : nrow=nrow+1
-        
-        for n in range(mn[0]):
-           ib   = 0
-           ie   = min(5,mn[1])
-           nstr = "%5g" % (n + 1)
 
-           f.write(' ETA=' + nstr+' '+ ' '.join(("%.8e" % x) for x in self.x[n][ib:ie]) + '\n')
-           for i in range(1,nrow):
-              ib = i*5
-              ie = min((i+1)*5,mn[1])
-              f.write('            ' + ' '.join(("%.8e" % x) for x in self.x[n][ib:ie]) + '\n')
-
-        for n in range(mn[0]):
-           ib   = 0
-           ie   = min(5,mn[1])
-           nstr = "%5g" % (n + 1)
-
-           f.write(' ETA=' + nstr+' '+ ' '.join(("%.8e" % x) for x in self.y[n][ib:ie]) + '\n')
-           for i in range(1,nrow):
-              ib = i*5
-              ie = min((i+1)*5,mn[1])
-              f.write('            ' + ' '.join(("%.8e" % x) for x in self.y[n][ib:ie]) + '\n')
-
-        f.close()
+class gsh:
+    """Unstructured triangular grid from Jigsaw
+    """
