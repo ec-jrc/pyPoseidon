9e7298d1b360bc6d36d24fbd583bde1c7c5cde33
brey <breyiannis@gmail.com>
2019-11-11 12:30:53 +0100

    Update test_meteo_grib.py

--- pyPoseidon/tests/test_meteo_grib.py
+++ pyPoseidon/tests/test_meteo_grib.py
@@ -1,4 +1,5 @@
 import pyPoseidon.meteo as pmeteo
+import pyPoseidon.model as pmodel
 import pytest
 import xarray as xr
 import os
@@ -10,12 +11,12 @@ def schism(tmpdir,name):
 
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', name)
     #read meteo file
-    df = pmeteo.cfgrib(filenames=filename)
-    df = df.sortby('latitude', ascending=True)
+    df = pmeteo.meteo(meteo_source=filename, engine='cfgrib')
+    df.Dataset = df.Dataset.sortby('latitude', ascending=True)
 
     rpath = str(tmpdir)+'/'
     #output to uvp files
-    pmeteo.to_output(df,solver='schism',rpath=rpath)
+    df.to_output(solver='schism',rpath=rpath)
 
     #read again meteo
     path = rpath + '/sflux/'
@@ -28,9 +29,9 @@ def schism(tmpdir,name):
 #        print ("Error: %s - %s." % (e.filename, e.strerror))
 
     #compare
-    msl = np.array_equal(df.msl.values,dr.prmsl.values)
-    u10 = np.array_equal(df.u10.values,dr.uwind.values)
-    v10 = np.array_equal(df.v10.values,dr.vwind.values)
+    msl = np.array_equal(df.Dataset.msl.values,dr.prmsl.values)
+    u10 = np.array_equal(df.Dataset.u10.values,dr.uwind.values)
+    v10 = np.array_equal(df.Dataset.v10.values,dr.vwind.values)
 
     return all([msl,u10,v10])
     
@@ -38,32 +39,34 @@ def d3d(tmpdir,name):
 
     filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', name)
     #read meteo file
-    df = pmeteo.cfgrib(filenames=filename)
+    df = pmeteo.meteo(meteo_source=filename, engine='cfgrib')
     
     rpath = str(tmpdir)+'/'
     #output to uvp files
-    pmeteo.to_output(df,solver='d3d',rpath=rpath)
+    df.to_output(solver='d3d',rpath=rpath)
 
     #read again meteo
-    p = pmeteo.read_d3d_meteo(rpath + 'p.amp', 'msl')
-    u = pmeteo.read_d3d_meteo(rpath + 'u.amu', 'u10')
-    v = pmeteo.read_d3d_meteo(rpath + 'v.amv', 'v10')
+    m = pmodel(solver='d3d')
+    
+    p = m.from_force(rpath + 'p.amp', 'msl')
+    u = m.from_force(rpath + 'u.amu', 'u10')
+    v = m.from_force(rpath + 'v.amv', 'v10')
     
     dr = xr.merge([p,u,v])
     dr = dr.sortby('latitude', ascending=True)
     
     #compare  
-    df = df.sortby('latitude', ascending=True)
+    df.Dataset = df.Dataset.sortby('latitude', ascending=True)
           
-    msl = np.abs(df.msl.values - dr.msl.values).max() < 1e-3
-    u10 = np.abs(df.u10.values - dr.u10.values).max() < 1e-3
-    v10 = np.abs(df.v10.values - dr.v10.values).max() < 1e-3
+    msl = np.abs(df.Dataset.msl.values - dr.msl.values).max() < 1e-3
+    u10 = np.abs(df.Dataset.u10.values - dr.u10.values).max() < 1e-3
+    v10 = np.abs(df.Dataset.v10.values - dr.v10.values).max() < 1e-3
 
     return all([msl,u10,v10])
     
     
     
-@pytest.mark.parametrize('filename', ['erai.grib', 'era5.grib', 'oper/uvp_2018100112.grib' ])
+@pytest.mark.parametrize('filename', ['erai.grib', 'era5.grib', 'uvp_2018100112.grib' ])
 def test_answer(tmpdir, filename):
     assert schism(tmpdir,filename) == True
     assert d3d(tmpdir,filename) == True
