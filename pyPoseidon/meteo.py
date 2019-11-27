ec-jrc/pyPoseidon

commit 9e4b1d1525b77562cf858f877f183b1a9581819f

meteo: Fix broken tests in to_output()
The problem was introduced in: d2868c8b52edc4bd204c08f6b15232cbbb627a0c

pyPoseidon/meteo.py
@@ -158,11 +158,8 @@ class meteo:
             
         s = getattr(model,solver) # get solver class
         
-        var_list = kwargs.get('vars', ['msl','u10','v10'])
-            
-        kwargs_ = kwargs.copy()
-        del kwargs_['vars']  
-            
+
+        var_list = kwargs.pop('vars', ['msl','u10','v10'])
         s.to_force(self.Dataset,vars=var_list, **kwargs_)
         
 

commit 59508f80d9c90207ce7c757035ea2700a176a530

meteo: Fix typo introduced in 9e4b1d1525b775

pyPoseidon/meteo.py
@@ -160,7 +160,7 @@ class meteo:
 
 
         var_list = kwargs.pop('vars', ['msl','u10','v10'])
-        s.to_force(self.Dataset,vars=var_list, **kwargs_)
+        s.to_force(self.Dataset,vars=var_list, **kwargs)
 
 
 def cfgrib(filenames=None, lon_min=None, lon_max=None, lat_min=None, lat_max=None, start_date=None, end_date=None, time_frame=None, irange=[0,-1,1], combine_forecast=False, combine_by='by_coords', **kwargs):