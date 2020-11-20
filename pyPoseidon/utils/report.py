981fe215c71861bbbf3a62d3450f631ef9cacf88
brey <breyiannis@gmail.com>
2020-11-20 18:08:24 +0100

    Update report.py
    Fix

--- pyPoseidon/utils/report.py
+++ pyPoseidon/utils/report.py
@@ -11,6 +11,9 @@ from folium import plugins
 import altair as alt
 import os
 import json
+from branca.element import MacroElement
+from jinja2 import Template
+
 import logging
 
 alt.data_transformers.disable_max_rows()
@@ -26,6 +29,34 @@ def source(tg,st):
     pdata.columns=['height']
     pdata = pdata.reset_index()
     return pdata
+
+
+
+class BindColormap(MacroElement):
+    """Binds a colormap to a given layer.
+
+    Parameters
+    ----------
+    colormap : branca.colormap.ColorMap
+        The colormap to bind.
+    """
+    def __init__(self, layer, colormap):
+        super(BindColormap, self).__init__()
+        self.layer = layer
+        self.colormap = colormap
+        self._template = Template(u"""
+        {% macro script(this, kwargs) %}
+            {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+            {{this._parent.get_name()}}.on('overlayadd', function (eventLayer) {
+                if (eventLayer.layer == {{this.layer.get_name()}}) {
+                    {{this.colormap.get_name()}}.svg[0][0].style.display = 'block';
+                }});
+            {{this._parent.get_name()}}.on('overlayremove', function (eventLayer) {
+                if (eventLayer.layer == {{this.layer.get_name()}}) {
+                    {{this.colormap.get_name()}}.svg[0][0].style.display = 'none';
+                }});
+        {% endmacro %}
+        """)  # noqa    
     
     
 def graph(source, title):
@@ -129,7 +160,7 @@ def to_html(path='./',tag='schism'):
     maxh = folium.GeoJson(
     gf_,
     style_function=lambda x: {
-        "fillColor": colormap(x['properties']['value']),
+        "fillColor": colormap(x['properties']['max_elev']),
         'weight':0,
         "fillOpacity": 1
     },
