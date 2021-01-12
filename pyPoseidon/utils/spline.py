79b35bf3d699f9aab1d6a1531e46590798d6d100
brey <breyiannis@gmail.com>
2021-01-12 10:55:35 +0100

    Update spline.py

--- pyPoseidon/utils/spline.py
+++ pyPoseidon/utils/spline.py
@@ -2,20 +2,38 @@ import pandas as pd
 import geopandas as gp
 import numpy as np
 import shapely
+import pygeos
+from scipy.interpolate import interp1d
 
-def spline(df):
+#based on https://stackoverflow.com/questions/52014197/how-to-interpolate-a-2d-curve-in-python
+    
+def spline(df, ds=0.001, method='slinear'):
+    
+    points = df.values  # a (nbre_points x nbre_dim) array
 
-    pol = gp.GeoDataFrame(geometry=[shapely.geometry.LineString(df.values)])
+    # Linear length along the line:
+    distance = np.cumsum( np.sqrt(np.sum( np.diff(points, axis=0)**2, axis=1 )) )
+    
+    # Choose n_points
+    ips = max(int(distance[-1]/ds), len(distance))
 
-    nls = len(pol.geometry[0].coords.xy[0])
-    nls
+    # normalize
+    distance = np.insert(distance, 0, 0)/distance[-1]
 
-    ips = 2*nls
-    ips
+    # Interpolation for different methods:
+#    interpolations_methods = ['slinear', 'quadratic', 'cubic']
 
-    gpf= gp.GeoDataFrame(geometry=[pol.geometry[0].interpolate(i/float(ips-1), normalized=True) for i in range(ips)])
+    alpha = np.linspace(0, 1, ips)
 
-    di=pd.DataFrame(np.array([np.array(p.geometry.xy[:]).flatten() for (l,p) in gpf.iterrows()]),columns=['lon','lat'])
-
-
+    interpolator =  interp1d(distance, points, kind=method, axis=0)
+    interpolated_points = interpolator(alpha)
+    
+    di=pd.DataFrame(interpolated_points, columns=['lon','lat'])
+    
+    di.iloc[0]=df.iloc[0]
+    di.iloc[-1]=df.iloc[-1]
+    
     return di
+
+
+    
