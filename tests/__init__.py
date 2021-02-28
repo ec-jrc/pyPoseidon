23da02f01e85195f867ecf833ed4d2b66499beeb
Panos Mavrogiorgos <pmav99@gmail.com>
2021-02-28 17:04:13 +0200

    tests: Download test data on the fly
    This will allows us to reduce the size of the repository.
    
    Fixes #40

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,7 @@ import pathlib
 ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
 TEST_DIR = ROOT_DIR / "tests"
 DATA_DIR = TEST_DIR / "data"
+ROTATED_DIR = DATA_DIR / "rotated"
+
+DATA_DIR.mkdir(exist_ok=True)
+ROTATED_DIR.mkdir(exist_ok=True)
