64a15ab81bf34d674345027c86473b3ff43a93bc
Panos Mavrogiorgos <pmav99@gmail.com>
2021-02-28 16:34:13 +0200

    tests: Separate testsuite from the pyPoseidon package
    Fixes #23

--- pyPoseidon/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from .. import DATA_DIR
---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import pathlib
+
+# specify package paths
+ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
+TEST_DIR = ROOT_DIR / "tests"
+DATA_DIR = TEST_DIR / "data"
