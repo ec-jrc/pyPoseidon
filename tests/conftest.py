44b6b121fcfd8e9e58a6097214d887caa766cde6
Panos Mavrogiorgos <pmav99@gmail.com>
2021-03-01 10:45:50 +0200

    tests: rename --slow to --runslow
    Just using the same convention everywhere

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,6 @@ import requests
 from . import DATA_DIR
 
 
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "slow: mark test to run only when --slow is used"
-    )
-    config.addinivalue_line(
-        "markers", "schism: mark test to run only when --runschism is used"
-    )
-    config.addinivalue_line(
-        "markers", "delft: mark test to run only when --rundelft is used"
-    )
-
-
 def pytest_addoption(parser):
     parser.addoption(
         "--runschism", action="store_true", default=False, help="run schism tests"
@@ -28,18 +16,18 @@ def pytest_addoption(parser):
         "--rundelft", action="store_true", default=False, help="run delft tests"
     )
     parser.addoption(
-        "--slow", action="store_true", default=False, help="run slow tests"
+        "--runslow", action="store_true", default=False, help="run slow tests"
     )
 
 
 def pytest_collection_modifyitems(config, items):
     should_run_schism = config.getoption("--runschism")
     should_run_delft = config.getoption("--rundelft")
-    should_run_slow = config.getoption("--slow")
+    should_run_slow = config.getoption("--runslow")
 
     skip_schism = pytest.mark.skip(reason="need --runshism option to run")
     skip_delft = pytest.mark.skip(reason="need --rundelft option to run")
-    skip_slow = pytest.mark.skip(reason="need --slow option to run")
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
 
     for item in items:
         if "schism" in item.keywords and not should_run_schism:
