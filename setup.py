a75ba460b4e9eb1ba3702d6cbce560d142f2c049
brey <breyiannis@gmail.com>
2021-04-09 09:40:22 +0200

    minor edits

--- setup.py
+++ setup.py
@@ -71,4 +71,4 @@ setup(name=DISTNAME,
       tests_require=TESTS_REQUIRE,
       url=URL,
       packages=find_packages(),
-      package_data={'pyPoseidon': ['misc/*','tests/data/*']})
+      package_data={'pyPoseidon': ['misc/*']})
