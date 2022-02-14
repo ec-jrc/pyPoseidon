e9ed5000347658a52f667d3c8e5d5c34af40e120
Panos Mavrogiorgos <pmav99@gmail.com>
2022-02-14 18:19:46 +0200

    minor

--- pyposeidon/utils/mplot.py
+++ pyposeidon/utils/mplot.py
@@ -17,7 +17,7 @@ import numpy as np
 import pandas as pd
 import subprocess
 from pyposeidon.utils.stereo import to_3d
-from itkwidgets import view, Viewer
+from itkwidgets import view
 import os
 import sys
 
