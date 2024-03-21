900e64bec776d44b3b6bb3cfb8f3ab4f9f99960a
Panos Mavrogiorgos <pmav99@gmail.com>
2024-03-21 19:24:52 +0200

    tests: Skip telemac tests if `telapy` is not available.
    Also cleanup the imports a bit.

--- tests/test_telemac.py
+++ tests/test_telemac.py
@@ -1,20 +1,15 @@
-import pytest
-import pyposeidon
-from pyposeidon.utils import data
 import os
-import multiprocessing
-
-# from . import DATA_DIR
 import pathlib
 
-# specify package paths
-ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
-TEST_DIR = ROOT_DIR / "tests"
-DATA_DIR = TEST_DIR / "data"
-ROTATED_DIR = DATA_DIR / "rotated"
+import pyposeidon
+import pytest
 
-DATA_DIR.mkdir(exist_ok=True)
-ROTATED_DIR.mkdir(exist_ok=True)
+from . import DATA_DIR
+
+try:
+    import telapy
+except ImportError:
+    pytest.skip(reason="requires telapy", allow_module_level=True)
 
 
 MESH_FILE = (DATA_DIR / "hgrid.gr3").as_posix()
