5c00dcc6b981a19e4477d459f67e24e857ccc9b3
tomsail <saillour.thomas@gmail.com>
2024-03-22 12:10:39 +0100

    [CI] added quantitative tests for telemac

--- tests/test_telemac.py
+++ tests/test_telemac.py
@@ -1,4 +1,9 @@
 import os
+import numpy as np
+import xarray as xr
+import multiprocessing
+
+# from . import DATA_DIR
 import pathlib
 
 import pyposeidon
@@ -70,27 +75,40 @@ case3 = {
 }
 
 
-def telemac(tmpdir, dic):
-    # initialize a model
-    rpath = str(tmpdir) + "/"
-    dic.update({"rpath": rpath})  # use tmpdir for running the model
-
-    b = pyposeidon.model.set(**dic)
-
-    b.execute()
-    b.results()
-    zarr_tar = b.rpath + "/outputs/out_2D.zarr.tar"
-    if os.path.exists(zarr_tar):
-        a = pyposeidon.model.read(rpath + b.module + "_model.json")  # read model
-        a.execute()
-        a.results()
-        zarr_tar = a.rpath + "/outputs/out_2D.zarr.tar"
-        return os.path.exists(zarr_tar)
-    else:
-        return False
-
-
 @pytest.mark.telemac
 @pytest.mark.parametrize("case", [case0, case1, case2, case3])
-def test_answer(tmpdir, case):
-    assert telemac(tmpdir, case) == True
+def test_telemac(tmpdir, case):
+    # initialize a model
+    rpath = str(tmpdir) + "/"
+    case.update({"rpath": rpath})  # use tmpdir for running the model
+
+    b = pyposeidon.model.set(**case)
+    b.create()
+    b.mesh.Dataset.type[:] = "closed"
+    b.output()
+    b.save()
+    b.run()
+    b.results()
+    zarr_tar = b.rpath + "/outputs/out_2D.zarr.tar"
+    assert os.path.exists(zarr_tar)
+    if os.path.exists(zarr_tar):
+        a = pyposeidon.model.read(rpath + b.module + "_model.json")  # read model
+        a.create()
+        a.mesh.Dataset.type[:] = "closed"
+        a.output()
+        a.save()
+        a.run()
+        a.results()
+        zarr_tar = a.rpath + "/outputs/out_2D.zarr.tar"
+        res = a.rpath + "/results_2D.slf"
+        assert os.path.exists(zarr_tar)
+        ds = xr.open_dataset(res, engine="selafin")
+        if "WH" in ds.variables:
+            max_WH = ds.WH.max().values
+            assert np.isclose(max_WH, 4.13, atol=1e-2)
+        elif "S" in ds.variables:
+            max_S = ds.S.max().values
+            print(max_S)
+            assert max_S < 1
+        else:
+            pass
