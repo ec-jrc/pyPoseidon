a9633aa63a0e3776be3039ef79e1d26d6e6c2b41
Panos Mavrogiorgos <pmav99@gmail.com>
2024-04-08 19:59:32 +0300

    ci: Run tests on 3.12 too

--- scripts/generate_envs.sh
+++ scripts/generate_envs.sh
@@ -7,18 +7,22 @@ set -xeuo pipefail
 ./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml   ./dependencies/binary.yml > ./environments/binary-p3.9.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml  ./dependencies/binary.yml > ./environments/binary-p3.10.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml  ./dependencies/binary.yml > ./environments/binary-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml  ./dependencies/binary.yml > ./environments/binary-p3.12.yml
 
 # Base
 ./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.9.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.10.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.12.yml
 
 # Viz
 ./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.9.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.10.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.12.yml
 
 # Full
 ./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.9.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.10.yml
 ./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.12.yml
