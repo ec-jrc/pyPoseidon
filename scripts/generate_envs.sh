a99197c3c102e5b76c60500b47395929b453ae83
Panos Mavrogiorgos <pmav99@gmail.com>
2024-07-16 22:14:51 +0300

    scripts: Remove empty lines from environment files
    It seems that `srsly`, i.e. the library we use in `merge_environments_yaml.py`
    started to add an extra empty line at the end of the files. This sed call
    removes that.

--- scripts/generate_envs.sh
+++ scripts/generate_envs.sh
@@ -4,25 +4,25 @@
 set -xeuo pipefail
 
 # Binary
-./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml   ./dependencies/binary.yml > ./environments/binary-p3.9.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml  ./dependencies/binary.yml > ./environments/binary-p3.10.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml  ./dependencies/binary.yml > ./environments/binary-p3.11.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml  ./dependencies/binary.yml > ./environments/binary-p3.12.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml   ./dependencies/binary.yml | sed '/^$/d' > ./environments/binary-p3.9.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml  ./dependencies/binary.yml | sed '/^$/d' > ./environments/binary-p3.10.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml  ./dependencies/binary.yml | sed '/^$/d' > ./environments/binary-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml  ./dependencies/binary.yml | sed '/^$/d' > ./environments/binary-p3.12.yml
 
 # Base
-./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.9.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.10.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.11.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml > ./environments/base-p3.12.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml | sed '/^$/d' > ./environments/base-p3.9.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml | sed '/^$/d' > ./environments/base-p3.10.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml | sed '/^$/d' > ./environments/base-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml | sed '/^$/d' > ./environments/base-p3.12.yml
 
 # Viz
-./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.9.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.10.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.11.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/viz-p3.12.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/viz-p3.9.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/viz-p3.10.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/viz-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/viz-p3.12.yml
 
 # Full
-./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.9.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.10.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.11.yml
-./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml > ./environments/full-p3.12.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.9.yml  ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/full-p3.9.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.10.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/full-p3.10.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.11.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/full-p3.11.yml
+./scripts/merge_environment_yaml.py ./dependencies/python3.12.yml ./dependencies/binary.yml ./dependencies/dev.yml ./dependencies/docs.yml ./dependencies/main.yml ./dependencies/viz.yml | sed '/^$/d' > ./environments/full-p3.12.yml
